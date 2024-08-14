77a739a83ddac5025beb11f37179343d4f782828
Marzag <korgoon@gmail.com>
2024-08-14 22:17:10 +0200

    CMA-7 added missing getter and setter for Employee

--- employee-service/src/main/java/com/gminds/employee_service/model/Employee.java
+++ employee-service/src/main/java/com/gminds/employee_service/model/Employee.java
@@ -11,6 +11,14 @@ public class Employee {
     String name;
     String surname;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
