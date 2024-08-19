2a7c42040ec58b39022639e308b0c78375b38c77
Marzag <korgoon@gmail.com>
2024-08-19 02:24:43 +0200

    CMA-8 Implementation of cucumber tests for Integration test, added Testcontainer for integration tests, added all scripts from application to test/resources/schema.sql to initialize testcontainer with database

--- employee-service/src/test/java/com/gminds/employee_service/steps/EmployeeSteps.java
+++ employee-service/src/test/java/com/gminds/employee_service/steps/EmployeeSteps.java
@@ -1,9 +1,16 @@
 package com.gminds.employee_service.steps;
 
 import com.gminds.employee_service.config.AuthResourceServerConfig;
+import com.gminds.employee_service.model.Department;
 import com.gminds.employee_service.model.Employee;
+import com.gminds.employee_service.model.Job;
 import com.gminds.employee_service.model.dtos.EmployeeDTO;
+import com.gminds.employee_service.model.enums.AgreementStatus;
 import com.gminds.employee_service.repository.EmployeeRepository;
+import com.gminds.employee_service.repository.JobRepository;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -17,22 +24,26 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+import javax.sql.DataSource;
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @ActiveProfiles("test")
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@Testcontainers
 @Import(AuthResourceServerConfig.class)
 public class EmployeeSteps {
 
@@ -40,13 +51,24 @@ public class EmployeeSteps {
     private static final String AUTH_URL = "http://localhost:9000";
     private static final String HEALTH_ENDPOINT = "/actuator/health";
     private static final String CREATE_EMPLOYEE_ENDPOINT = "/api/v1/employees";
-    private String token;
 
+
+    @Autowired
+    JobRepository jobRepository;
+    List<Job> jobs = new ArrayList<>();
+    private String token;
     @Autowired
     private EmployeeRepository employeeRepository;
     private Long employeeId;
 
+    @Autowired
+    private DataSource dataSource;
 
+    @Before
+    public void init() {
+
+        jobs = jobRepository.findAll();
+    }
 
 
     @Then("the employee service is running")
@@ -62,17 +84,17 @@ public class EmployeeSteps {
                     .toEntity(String.class);
 
 
-            assertEquals(200, response.getStatusCodeValue(), "Service is not running");
+            assertEquals(200, response.getStatusCode().value());
         } catch (RestClientException e) {
             fail("Service is not running: " + e.getMessage());
         }
     }
 
-    @When("I create a new employee with name {string} and position {string}")
-    public void iCreateANewEmployeeWithNameAndPosition(String name, String position) {
+    @When("I create a new employee with name {string}")
+    public void iCreateANewEmployeeWithNameAndPosition(String name) {
         String url = BASE_URL + CREATE_EMPLOYEE_ENDPOINT;
 
-        Map<String, Object> employeeData = properEmployeeDataB2B();
+        Map<String, Object> employeeData = properEmployeeDataB2B(name);
 
 
         RestClient restClient = RestClient.create();
@@ -85,20 +107,20 @@ public class EmployeeSteps {
                     .toEntity(EmployeeDTO.class);
 
             employeeId = response.getBody().id();
-            assertEquals(HttpStatus.CREATED, response.getStatusCode(), "Employee was not created successfully");
+            assertEquals(HttpStatus.CREATED.value(), response.getStatusCode().value());
         } catch (RestClientException e) {
 
             fail("Failed to create employee: " + e.getMessage());
         }
     }
 
-    @Then("the employee should be added to the database")
+    @Then("the employee should be added to the database with name {string}")
     @Transactional(readOnly = true)
-    public void theEmployeeShouldBeAddedToTheDatabase() {
-        // Zakładam, że pracownik ma unikalną nazwę i nazwisko
+    public void theEmployeeShouldBeAddedToTheDatabase(String name) {
         try {
             Employee employee = employeeRepository.findById(employeeId).orElse(null);
             assertTrue("Employee was not found in the database", employee != null);
+            assertEquals(name, "John Doe");
         } catch (Exception e) {
             fail("An error occurred while verifying the employee in the database: " + e.getMessage());
         }
@@ -117,7 +139,7 @@ public class EmployeeSteps {
                     .toEntity(String.class);
 
             HttpStatusCode statusCode = response.getStatusCode();
-            assertEquals(HttpStatus.OK, statusCode, "Authentication failed!");
+            assertEquals(HttpStatus.OK.value(), statusCode.value());
 
             // Ponieważ token jest płaskim stringiem, możemy go bezpośrednio przypisać
             token = response.getBody();
@@ -133,12 +155,14 @@ public class EmployeeSteps {
         return "Basic " + new String(encodedAuth);
     }
 
-    Map<String, Object> properEmployeeDataB2B() {
+    Map<String, Object> properEmployeeDataB2B(String name) {
+
+        String[] fullName = name.split(" ");
 
         Map<String, Object> employeeData = new HashMap<>();
         employeeData.put("id", null);
-        employeeData.put("name", "Jeroen");
-        employeeData.put("surname", "van Dijk");
+        employeeData.put("name", fullName[0]);
+        employeeData.put("surname", fullName[1]);
 
 // Job details
         Map<String, Object> jobData = new HashMap<>();
@@ -197,4 +221,195 @@ public class EmployeeSteps {
 
         return employeeData;
     }
+
+
+    @When("I create a new employee with the following details:")
+    public void iCreateANewEmployeeWithDetails(DataTable dataTable) {
+        Map<String, String> employeeDetails = dataTable.asMap(String.class, String.class);
+
+        String url = BASE_URL + CREATE_EMPLOYEE_ENDPOINT;
+
+        // Tworzenie struktury EmployeeDTO na podstawie przekazanych danych
+        Map<String, Object> employeeData = new HashMap<>();
+        employeeData.put("id", null);
+        employeeData.put("name", employeeDetails.get("name"));
+        employeeData.put("surname", employeeDetails.get("surname"));
+
+        // Job details
+        Map<String, Object> jobData = findOrCreateJob(employeeDetails.get("jobTitle"), employeeDetails.get("departmentName"));
+        employeeData.put("job", jobData);
+
+        // Department details
+        Map<String, Object> departmentData = findOrCreateDepartment(employeeDetails.get("departmentName"));
+        employeeData.put("department", departmentData);
+
+        // Agreements
+        Map<String, Object> agreementData = createAgreement(employeeDetails.get("salary"), employeeDetails.get("agreementType"));
+        employeeData.put("agreements", Collections.singletonList(agreementData));
+
+        // Certificates
+        Map<String, Object> certificateData = createCertificate(employeeDetails.get("certificateName"));
+        employeeData.put("certificates", Collections.singletonList(certificateData));
+
+        // Employment History
+        Map<String, Object> employmentHistoryData = createEmploymentHistory(employeeDetails.get("companyName"), jobData.get("title"));
+        employeeData.put("employmentHistory", Collections.singletonList(employmentHistoryData));
+
+        // Wysyłanie żądania do serwera
+        RestClient restClient = RestClient.create();
+        try {
+            ResponseEntity<EmployeeDTO> response = restClient.post()
+                    .uri(url)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                    .body(employeeData)
+                    .retrieve()
+                    .toEntity(EmployeeDTO.class);
+
+            assertNotNull(response.getBody().id(), "Employee ID should not be null");
+            employeeId = response.getBody().id();
+            assertEquals(HttpStatus.CREATED.value(), response.getStatusCode().value());
+        } catch (RestClientException e) {
+            fail("Failed to create employee: " + e.getMessage());
+        }
+    }
+
+    @Then("the employee should be added to the database with name {string} and surname {string}")
+    public void the_employee_should_be_added_to_the_database_with_name_and_surname(String name, String surname) {
+        try {
+            Employee employee = employeeRepository.findById(employeeId).orElse(null);
+            assertTrue("Employee was not found in the database", employee != null);
+            assertEquals(name, employee.getName());
+            assertEquals(surname, employee.getSurname());
+        } catch (Exception e) {
+            fail("An error occurred while verifying the employee in the database: " + e.getMessage());
+        }
+    }
+
+    private Map<String, Object> findOrCreateJob(String jobTitle, String departmentName) {
+        Job job = jobs.stream()
+                .filter(j -> j.getTitle().equals(jobTitle)
+                        && j.getDepartment().getName().equals(departmentName))
+                .findFirst().orElseThrow();
+        Map<String, Object> jobData = new HashMap<>();
+        jobData.put("id", job.getId()); // Zakładane ID stanowiska pracy
+        jobData.put("title", job.getTitle());
+        jobData.put("description", job.getDescription());
+        jobData.put("departmentId", job.getDepartment().getId());
+        return jobData;
+    }
+
+    private Map<String, Object> findOrCreateDepartment(String departmentName) {
+        Department department = jobs.stream()
+                .filter(job -> job.getDepartment().getName().equals(departmentName))
+                .findFirst().orElseThrow().getDepartment();
+        // Logika znajdowania lub tworzenia działu
+        Map<String, Object> departmentData = new HashMap<>();
+        departmentData.put("id", department.getId()); // Zakładane ID działu
+        departmentData.put("name", department.getName());
+        departmentData.put("description", department.getDescription());
+        return departmentData;
+    }
+
+    private Map<String, Object> createAgreement(String salary, String agreementType) {
+        Map<String, Object> agreementData = new HashMap<>();
+        agreementData.put("id", null);
+        agreementData.put("salary", Double.valueOf(salary));
+        agreementData.put("fromDate", LocalDate.now());
+        agreementData.put("toDate", LocalDate.now().plus(364, ChronoUnit.DAYS));
+        agreementData.put("status", "ACTIVE");
+        agreementData.put("agreementType", agreementType);
+        agreementData.put("paymentType", "PER_MONTH");
+        return agreementData;
+    }
+
+    private Map<String, Object> createAgreement(String salary, String agreementType, LocalDate startDate, LocalDate endDate) {
+        Map<String, Object> agreementData = new HashMap<>();
+        agreementData.put("id", null);
+        agreementData.put("salary", Double.valueOf(salary));
+        agreementData.put("fromDate", startDate);
+        agreementData.put("toDate", endDate);
+        agreementData.put("status", "ACTIVE");
+        agreementData.put("agreementType", agreementType);
+        agreementData.put("paymentType", "PER_MONTH");
+        return agreementData;
+    }
+
+    private Map<String, Object> createCertificate(String certificateName) {
+        Map<String, Object> certificateData = new HashMap<>();
+        certificateData.put("id", null);
+        certificateData.put("certificateName", certificateName);
+        certificateData.put("issueDate", LocalDate.now().minus(3, ChronoUnit.YEARS));
+        certificateData.put("expiryDate", LocalDate.now().plus(3, ChronoUnit.YEARS));
+        certificateData.put("issuedBy", "Certifying Body");
+        return certificateData;
+    }
+
+    private Map<String, Object> createEmploymentHistory(String companyName, Object jobName) {
+        Map<String, Object> employmentHistoryData = new HashMap<>();
+        employmentHistoryData.put("id", null);
+        employmentHistoryData.put("companyName", companyName);
+        employmentHistoryData.put("jobName", jobName);
+        employmentHistoryData.put("fromDate", LocalDate.now().minus(2, ChronoUnit.YEARS));
+        employmentHistoryData.put("toDate", LocalDate.now().minus(10, ChronoUnit.DAYS));
+        return employmentHistoryData;
+    }
+
+
+    @And("I update the agreements for these employees with the following details:")
+    public void iUpdateTheAgreementsForTheseEmployeesWithTheFollowingDetails(DataTable dataTable) {
+        List<Map<String, String>> agreements = dataTable.asMaps(String.class, String.class);
+
+        for (Map<String, String> agreementDetails : agreements) {
+            Employee employee = employeeRepository
+                    .findByNameAndSurnameWithAgreements(
+                            agreementDetails.get("name"),
+                            agreementDetails.get("surname"))
+                    .orElse(null);
+
+            String updateAgreementEndpoint = "/api/v1/employees/{employeeId}/agreement";
+            if (employee != null) {
+
+                String url = BASE_URL + updateAgreementEndpoint.replace("{employeeId}", employee.getId().toString());
+
+                Map<String, Object> agreementData = createAgreement(
+                        agreementDetails.get("newSalary"),
+                        employee.getAgreements()
+                                .getFirst()
+                                .getAgreementType().name()
+                );
+
+                RestClient restClient = RestClient.create();
+                try {
+                    restClient.post()
+                            .uri(url)
+                            .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                            .body(agreementData)
+                            .retrieve()
+                            .toEntity(Void.class);
+                    fail("Expected HttpStatusCodeException not thrown");
+                } catch (HttpStatusCodeException ex) {
+                    assertEquals(HttpStatus.BAD_REQUEST.value(), ex.getStatusCode().value());
+                }
+            } else {
+                fail("Employee not found: " + agreementDetails.get("name") + " " + agreementDetails.get("surname"));
+            }
+        }
+    }
+
+    @Then("New agreement for {string} {string} with salary {string} should be prevented")
+    public void newAgreementWithWithSalaryShouldBePrevented(String name, String surname, String salary) {
+        Employee employee = employeeRepository.findByNameAndSurnameWithAgreements(name, surname).orElse(null);
+        assertNotNull(employee, "Employee should exist in the database");
+
+        // Assert that there is no agreement with the specified salary for the given employee
+        long count = employee.getAgreements().stream()
+                .filter(
+                        agr -> Objects.equals(agr.getSalary(), Double.valueOf(salary))
+                                && (agr.getStatus().equals(AgreementStatus.ACTIVE) || agr.getStatus().equals(AgreementStatus.FUTURE))
+                ).count();
+        assertEquals(0, count);
+    }
 }
+
+
+
--- employee-service/src/test/resources/features/employee.feature
+++ employee-service/src/test/resources/features/employee.feature
@@ -1,7 +1,48 @@
 Feature: Employee Management
 
-    Scenario: Create a new employee
-        Given I am authenticated as "user" with password "password"
-        And the employee service is running
-        When I create a new employee with name "John Doe" and position "Developer"
-        Then the employee should be added to the database
+  Scenario Outline: Create a new employee with dynamic data
+    Given I am authenticated as "<username>" with password "<password>"
+    And the employee service is running
+    When I create a new employee with the following details:
+      | key             | value             |
+      | name            | <name>            |
+      | surname         | <surname>         |
+      | departmentName  | <department>      |
+      | jobTitle        | <jobTitle>        |
+      | salary          | <salary>          |
+      | agreementType   | <agreementType>   |
+      | certificateName | <certificateName> |
+      | companyName     | <companyName>     |
+
+    Then the employee should be added to the database with name "<name>" and surname "<surname>"
+    Examples:
+      | username | password | name | surname | department | jobTitle         | salary | agreementType | certificateName           | companyName              |
+      | user     | password | Adam | Johnson | IT         | Junior Developer | 5500   | B2B           | Java Professional         | Amsterdam Tech Solutions |
+      | user     | password | John | Doe     | IT         | Junior Developer | 5500   | B2B           | Java Professional         | Amsterdam Tech Solutions |
+      | user     | password | Jane | Smith   | HR         | HR Specialist    | 8000   | EMPLOYMENT    | Certified HR Professional | Global HR Solutions      |
+
+  Scenario Outline: Prevent from creating incorrect agreements
+    Given I am authenticated as "<username>" with password "<password>"
+    And the employee service is running
+    When I create a new employee with the following details:
+      | key             | value             |
+      | name            | <name>            |
+      | surname         | <surname>         |
+      | departmentName  | <department>      |
+      | jobTitle        | <jobTitle>        |
+      | salary          | <salary>          |
+      | agreementType   | <agreementType>   |
+      | certificateName | <certificateName> |
+      | companyName     | <companyName>     |
+
+    And I update the agreements for these employees with the following details:
+      | name   | surname   | newSalary   |
+      | <name> | <surname> | <newSalary> |
+
+    Then New agreement for "<name>" "<surname>" with salary "<newSalary>" should be prevented
+
+    Examples:
+      | username | password | name | surname  | department | jobTitle         | salary | agreementType | certificateName           | companyName              | newSalary |
+      | user     | password | Adam | Thompson | IT         | Junior Developer | 5500   | B2B           | Java Professional         | Amsterdam Tech Solutions | 26000     |
+      | user     | password | Anna | Testowa  | HR         | HR Specialist    | 6000   | EMPLOYMENT    | Certified HR Professional | Global HR Solutions      | 30000     |
+      | user     | password | Mark | Brown    | SALES      | Sales Executive  | 7000   | B2B           | Sales Mastery             | Sales International      | 40000     |
