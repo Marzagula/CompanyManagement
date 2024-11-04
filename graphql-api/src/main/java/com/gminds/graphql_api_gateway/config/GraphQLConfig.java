afcc3f895d7a827c02727d42ef82b3af2ad5db8c
Marzag <korgoon@gmail.com>
2024-11-04 12:08:19 +0100

    CMA-13 renamed module to graphql-api

---
+++ graphql-api/src/main/java/com/gminds/graphql_api_gateway/config/GraphQLConfig.java
@@ -0,0 +1,16 @@
+package com.gminds.graphql_api_gateway.config;
+
+import com.tailrocks.graphql.datetime.LocalDateTimeScalar;
+import graphql.scalars.ExtendedScalars;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+@Configuration
+public class GraphQLConfig {
+
+    @Bean
+    public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+        return wiringBuilder -> wiringBuilder.scalar(ExtendedScalars.GraphQLBigDecimal);
+    }
+}
